a55b42932e6b508082466c75b5fc491295586a5a
Camilin <cami.car.val@outlook.com>
2025-04-26 22:04:43 -0500

    burned the dir

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -29,7 +29,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/login': {
-        target: process.env.AUTH_API_ADDRESS || 'http://127.0.0.1:8081',
+        target: 'https://todos-api-a9apeufvaggbfjda.eastus-01.azurewebsites.net',
         secure: false
       },
       '/todos': {
