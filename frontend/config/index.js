9c31502a881f29ef323093f6e3d341e26ef62093
Camilin <cami.car.val@outlook.com>
2025-04-26 21:44:27 -0500

    intebntiona error

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -29,7 +29,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/login': {
-        target: process.env.AUTH_API_ADDRESS || 'http://127.0.0.1:8081',
+        target: 'http://127.0.0.1:8081',
         secure: false
       },
       '/todos': {
